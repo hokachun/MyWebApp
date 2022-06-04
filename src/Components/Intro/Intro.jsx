08004ee1007e370164eacac55537527852de2371
hokachun <galenhokachun@gmail.com>
2022-06-04 15:19:40 +0800

    Break line for intro

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -7,9 +7,9 @@ const Intro = () => {
     <div className='intro'>
         <div className='i-main'>
             <div className="i-name">
-                <span>Hi! I am William. Nice to meet you!</span>
-                <span>Bonjour! Je m’appelle William. C’est un plaisir de vous rencontrer!</span>
-                <span>I am currently a full-stack developer in a top-ranked investment bank. Now based in Hong Kong.</span>
+                <div><span>Hi! I am William.</span><span> Nice to meet you!</span></div>
+                <div><span>Bonjour! Je m’appelle William.</span><span>  C’est un plaisir de vous rencontrer!</span></div>
+                <div><span>I am currently a full-stack developer in a top-ranked investment bank. Now based in Hong Kong.</span></div>
             </div>
             {/* <button className="button i-button">Find me</button> */}
             <div className="i-icons">
